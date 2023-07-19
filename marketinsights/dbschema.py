a363b74847764191d707d8e6ac5eab33c533b1c4
cwilko <chris.wilkinson@uk.ibm.com>
2023-07-20 00:21:28 +0100

    Alembic initial migration

--- marketinsights/dbschema.py
+++ marketinsights/dbschema.py
@@ -9,7 +9,7 @@ class House(Base):
     __tablename__ = "houses"
 
     id = Column(String(10), primary_key=True, nullable=False)
-    created_on = Column(DateTime(), default=datetime.now)
+    date = Column(DateTime(), default=datetime.now)
     location = Column(String(128), nullable=False)
     region = Column(String(64), nullable=False)
     type = Column(String(32), nullable=False)
@@ -18,4 +18,4 @@ class House(Base):
     agent = Column(String(64), nullable=False)
     description = Column(String(512), nullable=False)
     sold = Column(String(4), nullable=False)
-    sold_on = Column(DateTime(), default=datetime.now)
+    datesold = Column(DateTime(), default=datetime.now)
